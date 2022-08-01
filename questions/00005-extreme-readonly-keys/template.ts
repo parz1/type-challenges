0629bceafb682d08788369758e9f4292e19c2054
parz1 <parz1zhou@gmail.com>
2022-08-01 23:30:50 +0800

    learn 005&006

--- questions/00005-extreme-readonly-keys/template.ts
+++ questions/00005-extreme-readonly-keys/template.ts
@@ -1,1 +1,4 @@
-type GetReadonlyKeys<T> = any
+type Equal<T, K> = T extends K ? (K extends T ? true : false) : false
+type GetReadonlyKeys<T> = keyof {
+  [K in keyof T as Equal<T[K], Readonly<T[K]>> extends true ? K : never]: T[K]
+}
--- questions/00006-hard-simple-vue/template.ts
+++ questions/00006-hard-simple-vue/template.ts
@@ -1,1 +1,13 @@
-declare function SimpleVue(options: any): any
+type GetComputed<T> = {
+  [K in keyof T]: T[K] extends () => infer Result ? Result : never
+}
+
+type Options<TData, TComputed, TMethods> = {
+  data: (this: void) => TData
+  computed: TComputed & ThisType<TData>
+  methods: TMethods & ThisType<TData & GetComputed<TComputed> & TMethods>
+}
+
+declare function SimpleVue<TData, TComputed, TMethods>(
+  options: Options<TData, TComputed, TMethods>
+): unknown
